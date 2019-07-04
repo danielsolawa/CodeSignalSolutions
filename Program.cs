8a122f17070b455eeddee82c527af3d50ec374e5
Daniel Solawa <>
2019-07-04 07:27:23 +0200

    Removed unused code.

--- Program.cs
+++ Program.cs
@@ -8,14 +8,6 @@ namespace CodeSignalSolutions
     {
         static void Main(string[] args)
         {
-            var s = "<table><tr><td>1</td><td>TWO</td></tr><tr><td>three</td><td>FoUr4</td></tr></table>";
-
-            string[] split = s.Split("</tr>");
-            string[] cells = split[0].Split("</td>");
-            foreach (var s1 in cells)
-            {
-                Console.WriteLine($"{s1}");
-            }
 
         }
     }
